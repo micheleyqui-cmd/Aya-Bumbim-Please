bbf27550bf33203f3ba8eb94c243db946e35056e
micheleyqui-cmd <micheleyqui@gmail.com>
2026-02-13 13:52:10 +0800

    Update gif.js

--- gif.js
+++ gif.js
@@ -1,1 +1,1 @@
-this.mainGif.src = "https://media.giphy.com/media/artj92V8o75VPL7AeQ/giphy.gif";
+this.mainGif.src = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDVidzV4Mmk0cTZiMXh6bjd5azhia21pMHc1ZDdjZml3a3ZybmVwdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/i17zHLTMt1SmJMv6Aw/giphy.gif";
